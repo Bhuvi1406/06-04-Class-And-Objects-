d7488a571bef78a23a666b23c9e90a3d1e0da248
Bhuvi1406 <118524851+Bhuvi1406@users.noreply.github.com>
2024-04-08 23:08:06 +0530

    Add files via upload

---
+++ 08-04.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self):
+        self.name="a"
+        self.registernumber="2s"
+
+    def display(self):
+        print("Name",self.name)
+        print("Name",self.registernumber)
+
+name=student()
+
+        
+        
